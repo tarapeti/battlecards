a2d265e92abbf57b7556431206c7177c0f40e5a6
Gergő Papp <imgergopapp@gmail.com>
2019-02-12 15:03:23 +0100

    Added own test prints

--- battlecards/src/main/java/com/codecool/Main.java
+++ battlecards/src/main/java/com/codecool/Main.java
@@ -6,10 +6,19 @@ public class Main {
     public static void main(String[] args){
 
     Table table = new Table();
-    table.play();
+    //table.play();
+
+    Deck testDeck = new Deck();
+    List<Card> testCardList = testDeck.getDeck();
+    Comparator testCompare = new Comparator();
+    Table testTable = new Table();
+
+    //TEST CompareCards
+    Card testWinnerCard = testCompare.compareCards(testCardList,"speed");
+    System.out.println(testWinnerCard.getName() + " "+  testWinnerCard.getMaxSpeed());
 
 
-
+    //TEST incWinnerPoint
+    //testCompare.incWinnerPoints( ,testWinnerCard);
     }
-
-    }
+}
