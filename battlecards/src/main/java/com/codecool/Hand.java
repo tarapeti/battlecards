8e58e52e02430d167d960ef01aa6183046ccdb75
Gergő Papp <imgergopapp@gmail.com>
2019-02-12 17:32:29 +0100

    Fixed semantic error

--- battlecards/src/main/java/com/codecool/Hand.java
+++ battlecards/src/main/java/com/codecool/Hand.java
@@ -9,18 +9,15 @@ public class Hand {
     public void giveHands(List<Card> deck) {
         List<Card> deck1Deck = deck;
         List<Card> smallDeck = new ArrayList<>();
-        for(int i = 0; i < deck1Deck.size();){
+        for(int i = 0; i < deck1Deck.size(); i++){
             smallDeck.add(deck1Deck.get(i));
             if(smallDeck.size() == 5){
                 hand.add(smallDeck);
                 smallDeck = new ArrayList<>();
-                i = i + 5;
-            }
-            else {
-                i++;
             }
         }
     }
+
 
     public List<List<Card>> getHand() {
         return hand;
